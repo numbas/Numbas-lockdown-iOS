ae37dacdc12788749b6ab7095eca096c6d5e3d9f
Christian Lawson-Perfect <christian.perfect@ncl.ac.uk>
2022-10-11 14:26:46 +0100

    Initial Commit

---
+++ Numbas lockdown/Numbas_lockdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Numbas_lockdownApp.swift
+//  Numbas lockdown
+//
+//  Created by ncp34 on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Numbas_lockdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
